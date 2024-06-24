ad09396285003ff0f797dce1f20f55b4f1f03e15
Oluwatimilehin AjeOjo <110703714+timiajeojo@users.noreply.github.com>
2024-06-24 12:58:10 +0100

    add javascript

---
+++ index.js
@@ -0,0 +1,11 @@
+//for the DOM
+document.addEventListener("DOMContentLoaded", function() {
+    //get element by its id
+    let saveButton  = document.getElementById("saveButton")
+//for the click 
+    saveButton.addEventListener ("click", function() {
+        //log out
+        console.log("ButtonClicked");
+    })
+    
+})
