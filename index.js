162cedbad1798095cf74f7b646606f0877e0ca6d
Oluwatimilehin AjeOjo <110703714+timiajeojo@users.noreply.github.com>
2024-06-28 18:35:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-  let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
-  const inputEl = document.getElementById("input-el")
-  const inputBtn = document.getElementById("input-btn")
-
-  inputBtn.addEventListener("click", function() {
+inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads);
+    console.log(myLeads)
 })
 
+// Render the leads in the unordered list using ulEl.textContent
 for (let i = 0; i < myLeads.length; i++) {
-    console.log(myLeads[i])
-  }
+    ulEl.textContent += myLeads[i] + " "
+}
